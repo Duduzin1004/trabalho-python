247734e010872bf204a64dac34adb184feaebb34
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-03-10 08:46:25 -0300

    Update python.py

--- python.py
+++ python.py
@@ -21,3 +21,34 @@ if valida_maior:
     print("é maior de idade")
 elif valida_menor:
     print("é menor de idade")
+
+Senha correta
+
+senha=input("digite sua senha")
+valida_senha_correta='python123'==senha
+if valida_senha_correta:
+    print("acesso permitido")
+else:
+    print("acesso negado")
+
+Aprovação
+
+numero=input("digite sua nota:")
+numero=float(numero)
+valida_aprovado=numero>6.0
+valida_reprovado=numero<6.0
+if valida_aprovado:
+    print("aprovado")
+elif valida_reprovado:
+    print("reprovado")
+
+Desconto
+
+compra=input("digite o valor da compra: $")
+numero=float(numero)
+valida_valor_desconto=valor>=100
+esconto=valor * 0.10
+if valida_valor_desconto:
+    print("valor original: $)
+elif valida_valor normal:
+    print

09a9848573de7b3753795516060c0e033a7e92d4
Duduzin1004 <cruz.rocha.eduardo@escola.pr.gov.br>
2026-03-10 08:15:20 -0300

    Update python.py

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
+1 Número positivo ou negativo
+
 numero=input("digite um numero:")
 numero=int(numero)
 valida_pos=numero>0
@@ -8,3 +10,14 @@ elif valida_neg:
     print("o numero é negativo")
 else:
     print("o numero é zero")
+
+2 Maior de idade
+
+numero=input("digite um numero:")
+numero=int(numero)
+valida_maior=numero>=18
+valida_menor=numero<18
+if valida_maior:
+    print("é maior de idade")
+elif valida_menor:
+    print("é menor de idade")
